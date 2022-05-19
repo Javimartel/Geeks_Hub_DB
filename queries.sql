59720fd7e25bfbf737b67456a76820d3340a3d6e
Sstark97 <aitorscinfo@gmail.com>
2022-05-19 20:53:01 +0100

    add some queries

--- queries.sql
+++ queries.sql
@@ -3,6 +3,9 @@
 -- Consulta 1: mostrar todas las películas cuyo productor sea Netflix
 
 -- Consulta 2: mostrar a todas las cuentas "Premium"
+SELECT *
+FROM Cuenta
+WHERE Tipo_Suscripcion = "Premium";
 
 -- Consulta 3: mostrar todas las películas de "Ciencia Ficción"
 
@@ -15,6 +18,20 @@
 -- Consultas Compuestas
 
 -- Consulta 1: mostrar la lista de favoritos del perfil 4
+SELECT Cod_Contenido, Titulo, Genero
+FROM Favoritos F
+INNER JOIN Contenido_Favorito Cf
+ON F.Cod_Favoritos = Cf.Cod_Favoritos
+INNER JOIN Pelicula P
+ON Cf.Cod_Contenido = P.Cod_Pelicula
+UNION 
+SELECT Cod_Contenido, Titulo, Genero
+FROM Favoritos F
+INNER JOIN Contenido_Favorito Cf
+ON F.Cod_Favoritos = Cf.Cod_Favoritos
+INNER JOIN Serie S
+ON Cf.Cod_Contenido = S.Cod_Serie;
+
 
 -- Consulta 2: mostrar el historial del perfil 9
 
