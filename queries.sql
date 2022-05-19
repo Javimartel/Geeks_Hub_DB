9d52bbc0b47e8bff0a938f62a82161f42fbc59db
SaraCS21 <saracs15.scssn@gmail.com>
2022-05-19 20:37:37 +0200

    Add queries description

--- queries.sql
+++ queries.sql
@@ -1,1 +1,25 @@
 -- Consultas Simples
+
+-- Consulta 1: mostrar todas las películas cuyo productor sea Netflix
+
+-- Consulta 2: mostrar a todas las cuentas "Premium"
+
+-- Consulta 3: mostrar todas las películas de "Ciencia Ficción"
+
+-- Consulta 4: *Consultas Javi*
+
+-- Consulta 5: *Consultas Javi*
+
+------------
+
+-- Consultas Compuestas
+
+-- Consulta 1: mostrar la lista de favoritos del perfil 4
+
+-- Consulta 2: mostrar el historial del perfil 9
+
+-- Consulta 3: mostrar los perfiles de "Javier"
+
+-- Consulta 4: *Consultas Javi*
+
+-- Consulta 5: *Consultas Javi*
