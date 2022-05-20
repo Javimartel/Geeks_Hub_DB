9ded0b4abd2034687019f29f6dd562d6d6256021
Sstark97 <aitorscinfo@gmail.com>
2022-05-20 01:12:23 +0100

    Add Serie Table

--- streaming_sqlite.sql
+++ streaming_sqlite.sql
@@ -19,3 +19,31 @@ CREATE TABLE Cuenta (
     Tipo_Suscripcion TEXT CHECK (Tipo_Suscripcion IN ("Basico", "Estandar", "Premium")),
     FOREIGN KEY (Tipo_Suscripcion) REFERENCES Suscripcion(Tipo_Suscripcion) ON DELETE CASCADE
 );
+
+CREATE TABLE Serie (
+    Cod_Serie TEXT,
+    N_Temporada INTEGER,
+    Titulo TEXT NOT NULL,
+    Calificacion_Edad TEXT NOT NULL CHECk (Calificacion_Edad IN ("tp", "8", "12", "16", "18")),
+    Genero TEXT NOT NULL CHECK (Genero IN ("Suspense", "Terror", "Comedia", "Accion", "Drama", "Fantasia", "Romance", "Aventura", "Ciencia Ficcion", "Belico", "Musical", "Documental") ),
+    Director TEXT NOT NULL,
+    Puntuacion_Media REAL DEFAULT 0,
+    Productor REAL NOT NULL,
+    Sinopsis TEXT NOT NULL, 
+    Fecha_Publicacion TEXT NOT NULL,
+    Portada TEXT NOT NULL,
+    Trailer TEXT NOT NULL,
+    Capitulos INTEGER NOT NULL,
+    PRIMARY KEY(Cod_Serie, N_Temporada)
+);
+
+CREATE TABLE Perfil (
+    Cod_Perfil TEXT,
+    Correo TEXT,
+    Nickname TEXT NOT NULL,
+    Imagen TEXT CHECK (Imagen IN ("Imagen1", "Imagen2", "Imagen3")),
+    Cod_Favoritos TEXT,
+    PRIMARY KEY(Cod_Perfil, Correo),
+    FOREIGN KEY (Correo) REFERENCES Cuenta(Correo) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (Cod_Favoritos) REFERENCES Favoritos(Cod_Favoritos) ON UPDATE CASCADE ON DELETE CASCADE
+);
